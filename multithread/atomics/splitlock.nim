d9c6a32df2a6402b26064eab26adda57ce5705cf
planetis-m <b3lievem@yandex.com>
2021-07-07 13:36:04 +0300

    fix

--- multithread/atomics/splitlock.nim
+++ multithread/atomics/splitlock.nim
@@ -1,15 +1,16 @@
 # https://rigtorp.se/split-locks/
-import times
+# perf stat -e sq_misc.split_lock ./splitlock
+import std/[times, strformat]
 
 proc main =
   var buf: array[128, char]
-  var v = cast[ptr uint32](cast[uint64](buf) or 61)
+  var v = cast[ptr uint32](cast[uint64](addr buf) or 61)
 
   let start = epochTime()
   const numIters = 1_000_000
   for i in 0 ..< numIters:
     discard atomicFetchAdd(v, 1, AtomicAcquire)
   let stop = epochTime()
-  echo (stop - start) / numIters.float
+  echo &"{(stop - start) * 1000:4.4f} ns per operation"
 
 main()
