8aa2589d63a3384daa1b633b6b116dee2d212729
Antonis Geralis <b3lievem@yandex.com>
2022-07-24 16:59:26 +0300

    fix toSet, signed promotion

--- utils/safesets.nim
+++ utils/safesets.nim
@@ -1,13 +1,13 @@
 import std/setutils
 
-proc toSet*[T: SomeInteger; E](x: T, t: typedesc[E]): set[E] {.inline.} =
+proc toSet*[T: SomeUnsignedInt; E](x: T, t: typedesc[E]): set[E] {.inline.} =
   if (x and cast[T](fullSet(E))) == x: result = cast[set[E]](x)
-  else: raise newException(ValueError, $x & " can't be converted to " & $t)
+  else: raise newException(ValueError, $x & " can't be converted to a set of " & $t)
 
 when isMainModule:
   type
     Foo = enum
-      bar, baz, baf
+      bar, baz = 7, baf = 15
 
-  let x = toSet(8, Foo)
+  let x = toSet(128, Foo)
   echo x
