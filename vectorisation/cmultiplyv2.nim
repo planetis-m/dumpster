2c821621df4aca121cebb3db6495b3bf17dcea4e
Antonis Geralis <b3lievem@yandex.com>
2023-04-28 22:29:14 +0300

    simplify

--- vectorisation/cmultiplyv2.nim
+++ vectorisation/cmultiplyv2.nim
@@ -5,32 +5,28 @@ type
 
 macro multiplyImpl(M, N, K: static[int]; a, b, res: typed): untyped =
   result = buildAst(stmtList):
-    var aN = newNimNode(nnkBracket)
+    var an = newNimNode(nnkBracket)
     for i in 0 ..< M*K:
-      aN.add genSym(nskLet, "tmp")
-      newLetStmt(aN[i], newTree(nnkBracketExpr, a, newLit(i)))
-    var bN = newNimNode(nnkBracket)
+      an.add genSym(nskLet, "a")
+      newLetStmt(an[i], newTree(nnkBracketExpr, a, newLit(i)))
+    var bn = newNimNode(nnkBracket)
     for i in 0 ..< K*N:
-      bN.add genSym(nskLet, "tmp")
-      newLetStmt(bN[i], newTree(nnkBracketExpr, b, newLit(i)))
-    var cN = newNimNode(nnkBracket)
+      bn.add genSym(nskLet, "b")
+      newLetStmt(bn[i], newTree(nnkBracketExpr, b, newLit(i)))
+    var cn = newNimNode(nnkBracket)
     for i in 0 ..< M*N:
-      cN.add genSym(nskLet, "tmp")
+      cn.add genSym(nskLet, "c")
     for j in 0 ..< N:
       for i in 0 ..< M:
         letSection:
           identDefs:
-            cN[i * N + j]
+            cn[i * N + j]
             empty()
             let args = buildAst(bracket):
               for k in 0 ..< K:
-                infix(bindSym"*", aN[i * K + k], bN[k * N + j])
+                infix(bindSym"*", an[i * K + k], bn[k * N + j])
             nestList(bindSym"+", args)
-    let ret = buildAst(returnStmt):
-      bracket:
-        for i in 0 ..< M*N:
-          cN[i]
-    ret
+    returnStmt(cn)
 
 proc `*`*[M, N, K: static[int]](a: Matrix[M, K], b: Matrix[K, N]): Matrix[M, N] =
   var c: Matrix[M, N]
