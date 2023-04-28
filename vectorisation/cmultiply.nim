7d4ae086773b015bb5861eee4e07b26a4b452799
Antonis Geralis <b3lievem@yandex.com>
2023-04-28 22:24:41 +0300

    keep it simple

--- vectorisation/cmultiply.nim
+++ vectorisation/cmultiply.nim
@@ -1,28 +1,23 @@
 import std/macros, fusion/astdsl
 
 type
-  Matrix*[M, N: static[int]] = array[M * N, float32]
+  Matrix*[M, N: static[int]] = array[M * N, int32]
 
 macro multiplyImpl(M, N, K: static[int]; a, b, res: typed): untyped =
   result = buildAst(stmtList):
     for j in 0 ..< N:
       for i in 0 ..< M:
-        asgn:
-          bracketExpr(res, intLit(intVal = i * N + j))
-          let args = buildAst(bracket):
-            for k in 0 ..< K:
-              infix(bindSym"*", bracketExpr(a, intLit(intVal = i * K + k)),
-                  bracketExpr(b, intLit(intVal = k * N + j)))
-          nestList(bindSym"+", args)
+        for k in 0 ..< K:
+          infix(bindSym"+=", bracketExpr(res, intLit(intVal = i * N + j)),
+                infix(bindSym"*", bracketExpr(a, intLit(intVal = i * K + k)),
+                      bracketExpr(b, intLit(intVal = k * N + j))))
 
-proc `*`*[M, N, K: static[int]](a: Matrix[M, K], b: Matrix[K, N]): Matrix[M, N] {.inline.} =
-  var c: Matrix[M, N]
-  multiplyImpl(M, N, K, a, b, c)
-  return c
+proc `*`*[M, N, K: static[int]](a: Matrix[M, K], b: Matrix[K, N]): Matrix[M, N] =
+  multiplyImpl(M, N, K, a, b, result)
 
 when isMainModule:
   var
-    a: Matrix[4, 3] = [1'f32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    b: Matrix[3, 5] = [1'f32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    a: Matrix[4, 3] = [1'i32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    b: Matrix[3, 5] = [1'i32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     c = a * b
   echo c
