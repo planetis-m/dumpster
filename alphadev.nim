78f761a73d30ff425da65e2cbc9203c46fcfa013
Antonis Geralis <b3lievem@yandex.com>
2023-06-07 20:55:52 +0300

    alphadev

---
+++ alphadev.nim
@@ -0,0 +1,99 @@
+# // Ensures that __c(*__x, *__y) is true by swapping *__x and *__y if necessary.
+# void cond_swap(int* x, int* y, bool (*c)(int, int)) {
+#   bool r = c(*x, *y);
+#   int tmp = r ? *x : *y;
+#   *y = r ? *y : *x;
+#   *x = tmp;
+# }
+
+# // Ensures that *__x, *__y and *__z are ordered according to the comparator __c,
+# // under the assumption that *__y and *__z are already ordered.
+# void partially_sorted_swap(int* x, int* y, int* z, bool (*c)(int, int)) {
+#   bool r = c(*z, *x);
+#   int tmp = r ? *z : *x;
+#   *z = r ? *x : *z;
+#   r = c(tmp, *y);
+#   *x = r ? *x : *y;
+#   *y = r ? *y : tmp;
+# }
+
+# void sort3_maybe_branchless(int* x1, int* x2, int* x3,
+#                          bool (*c)(int, int)) {
+#   cond_swap(x2, x3, c);
+#   partially_sorted_swap(x1, x2, x3, c);
+# }
+
+# void sort4_maybe_branchless(int* x1, int* x2, int* x3,
+#                          int* x4, bool (*c)(int, int)) {
+#   cond_swap(x1, x3, c);
+#   cond_swap(x2, x4, c);
+#   cond_swap(x1, x2, c);
+#   cond_swap(x3, x4, c);
+#   cond_swap(x2, x3, c);
+# }
+
+# void sort5_maybe_branchless(int* x1, int* x2, int* x3,
+#                          int* x4, int* x5, bool (*c)(int, int)) {
+#   cond_swap(x1, x2, c);
+#   cond_swap(x4, x5, c);
+#   partially_sorted_swap(x3, x4, x5, c);
+#   cond_swap(x2, x5, c);
+#   partially_sorted_swap(x1, x3, x4, c);
+#   partially_sorted_swap(x2, x3, x4, c);
+# }
+
+# bool compare(int a, int b) {
+#     return a > b;
+# }
+
+proc cond_swap(x: var int, y: var int, c: proc (a, b: int): bool {.nimcall.}) =
+  let r = c(x, y)
+  let tmp = if r: x else: y
+  y = if r: y else: x
+  x = tmp
+
+proc partially_sorted_swap(x: var int, y: var int, z: var int, c: proc (a, b: int): bool {.nimcall.}) =
+  var r = c(z, x)
+  let tmp = if r: z else: x
+  z = if r: x else: z
+  r = c(tmp, y)
+  x = if r: x else: y
+  y = if r: y else: tmp
+
+proc sort3_maybe_branchless(x1: var int, x2: var int, x3: var int, c: proc (a, b: int): bool {.nimcall.}) =
+  cond_swap(x2, x3, c)
+  partially_sorted_swap(x1, x2, x3, c)
+
+proc sort4_maybe_branchless(x1: var int, x2: var int, x3: var int, x4: var int, c: proc (a, b: int): bool {.nimcall.}) =
+  cond_swap(x1, x3, c)
+  cond_swap(x2, x4, c)
+  cond_swap(x1, x2, c)
+  cond_swap(x3, x4, c)
+  cond_swap(x2, x3, c)
+
+proc sort5_maybe_branchless(x1: var int, x2: var int, x3: var int, x4: var int, x5: var int, c: proc (a, b: int): bool {.nimcall.}) =
+  cond_swap(x1, x2, c)
+  cond_swap(x4, x5, c)
+  partially_sorted_swap(x3, x4, x5, c)
+  cond_swap(x2, x5, c)
+  partially_sorted_swap(x1, x3, x4, c)
+  partially_sorted_swap(x2, x3, x4, c)
+
+proc compare(a: int, b: int): bool = a < b
+
+import std/strformat
+
+proc main() =
+  var arr1 = [5, -2, 9]
+  sort3_maybe_branchless(arr1[0], arr1[1], arr1[2], compare)
+  echo &"Sorted array: {arr1[0]} {arr1[1]} {arr1[2]}"
+
+  # var arr2 = [-1, 8, 3, -6]
+  # sort4_maybe_branchless(arr2[0], arr2[1], arr2[2], arr2[3], compare)
+  # echo &"Sorted array: {arr2[0]} {arr2[1]} {arr2[2]} {arr2[3]}"
+  #
+  # var arr3 = [2, -7, 10, 4, -3]
+  # sort5_maybe_branchless(arr3[0], arr3[1], arr3[2], arr3[3], arr3[4], compare)
+  # echo &"Sorted array: {arr3[0]} {arr3[1]} {arr3[2]} {arr3[3]} {arr3[4]}"
+
+main()
