053d7194c7edf75f4f1b28f3ac208e4232563f31
Antonis Geralis <b3lievem@yandex.com>
2023-07-08 23:34:01 +0300

    add buckets

--- webapps/token_bucket.nim
+++ webapps/token_bucket.nim
@@ -1,35 +1,38 @@
-import std/times, os
+import std/[times, tables]
 
 type
-  TokenBucket = object
-    capacity, tokens, refillRate: float
-    lastRefill: Time
+  TokenBucket[K] = object
+    capacity, refillRate: float
+    buckets: Table[K, tuple[tokens: float, lastRefillTime: float]]
 
-proc refill(tb: var TokenBucket) =
-  let now = getTime()
-  let elapsedSeconds = now - tb.lastRefill
-  let refillAmount = elapsedSeconds.inSeconds.float * tb.refillRate # This is problematic...
-  tb.tokens = min(tb.capacity, tb.tokens + refillAmount)
-  tb.lastRefill = now
+proc refill[K](tb: var TokenBucket[K]; key: K) =
+  if tb.buckets.hasKeyOrPut(key, (tb.capacity, epochTime())):
+    let (tokens, lastRefillTime) = tb.buckets[key]
+    let now = epochTime()
+    let elapsedSeconds = now - lastRefillTime
+    let refillAmount = elapsedSeconds * tb.refillRate
+    tb.buckets[key] = (min(tb.capacity, tokens + refillAmount), now)
 
-proc consume(tb: var TokenBucket; tokens: float): bool =
-  tb.refill()
-  if tokens <= tb.tokens:
-    tb.tokens -= tokens
+proc consume[K](tb: var TokenBucket[K]; key: K; tokens: float): bool =
+  tb.refill(key)
+  if tokens <= tb.buckets[key].tokens:
+    tb.buckets[key].tokens -= tokens
     true
   else:
     false
 
-proc newTokenBucket(capacity, refillRate: float): TokenBucket =
-  TokenBucket(capacity: capacity, tokens: capacity, refillRate: refillRate, lastRefill: getTime())
+proc newTokenBucket[K](capacity, refillRate: float): TokenBucket[K] =
+  TokenBucket[K](capacity: capacity, refillRate: refillRate)
+
+import std/os
 
 var
-  tokenBucket = newTokenBucket(10, 1)
+  tokenBucket = newTokenBucket[string](10, 1)
 
 var count = 0
 for i in 1..180:
   sleep(400)
-  var res = tokenBucket.consume(1)
+  var res = tokenBucket.consume("a", 1)
   if res: inc count
   echo res
 echo count
