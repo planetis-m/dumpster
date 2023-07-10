568310ca6338ec5b7dac2613d7837e5c97b81bbf
Antonis Geralis <b3lievem@yandex.com>
2023-07-10 19:05:38 +0300

    add simpler alternative

--- gameloop.nim
+++ gameloop.nim
@@ -30,3 +30,28 @@ proc run(game: var Game) =
     if framesSkipped > 0:
       let alpha = accumulator.float32 / skippedTicks / 1_000_000_000
       game.render(alpha)
+
+# proc run(game: var Game) =
+#   const
+#     ticksPerSec = 25
+#     skippedTicks = 1_000_000_000 div ticksPerSec # to nanosecs per tick
+#     maxDelta = skippedTicks * 2
+#
+#   var
+#     lastTime = getMonoTime().ticks
+#     accumulator = 0
+#
+#   while true:
+#     handleEvents(game)
+#     if not game.isRunning: break
+#
+#     let now = getMonoTime().ticks
+#     accumulator += min(now - lastTime, maxDelta)
+#     lastTime = now
+#
+#     while accumulator >= skippedTicks:
+#       game.update()
+#       accumulator -= skippedTicks
+#
+#     let alpha = accumulator.float32 / skippedTicks / 1_000_000_000
+#     game.render(alpha)
