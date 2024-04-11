7f276a6b484bd11b4657c0e7a0a5d3d30df9714a
Antonis Geralis <b3lievem@yandex.com>
2024-04-11 12:07:32 +0300

    comments

--- classical_ml/decision_tree.nim
+++ classical_ml/decision_tree.nim
@@ -43,25 +43,30 @@ const
   }
 
 proc generateNode(nodeData: JsonTree, input: NimNode): NimNode =
-  let res = ident"result"
   if nodeData.contains(JsonPtr"/class"):
-    result = newAssignment(res, newLit(fromJson(nodeData, JsonPtr"/class", TargetNames)))
+    # result = TargetNames(class)
+    result = newAssignment(ident"result", newLit(fromJson(nodeData, JsonPtr"/class", TargetNames)))
   else:
     result = newNimNode(nnkIfStmt)
+    # if input[feature] <= threshold'f32:
     result.add(newTree(nnkElifBranch,
         infix(newTree(nnkBracketExpr, input, newLit(fromJson(nodeData, JsonPtr"/feature", FeatureNames).int)),
         "<=", newLit(fromJson(nodeData, JsonPtr"/threshold", float32))),
         generateNode(extract(nodeData, JsonPtr"/left"), input)))
+    # else:
     result.add(newTree(nnkElse, generateNode(extract(nodeData, JsonPtr"/right"), input)))
 
 macro generateClassifier(classifierData: static JsonTree): untyped =
   let input = genSym(nskParam, "input")
+  # proc predict(input: openarray[float32]): TargetNames =
   result = newProc(name = ident"predict",
       params = [bindSym"TargetNames", newIdentDefs(name = input,
-      kind = newTree(nnkBracketExpr, bindSym"seq", bindSym"float32"))],
+      kind = newTree(nnkBracketExpr, bindSym"openarray", bindSym"float32"))],
       body = generateNode(extract(classifierData, JsonPtr"/node"), input))
   echo result.repr
 
 generateClassifier(classifier)
 
-echo predict(@[4.9'f32,3.1,1.5,0.1]) # == setosa
+# echo predict(@[4.9'f32,3.1,1.5,0.1]) # == setosa
+# echo predict(@[6.7'f32,3.0,5.0,1.7]) # == versicolor (fail)
+# echo predict(@[7.7'f32,2.6,6.9,2.3]) # == virginica
--- classical_ml/random_forest.nim
+++ classical_ml/random_forest.nim
@@ -7,15 +7,17 @@ type
 const classifier = parseJson(readFile"rforest_min.json")
 
 proc generateNode(nodeData: JsonTree, input: NimNode): NimNode =
-  let res = ident"result"
   if nodeData.contains(JsonPtr"/class"):
-    result = newAssignment(res, newLit(fromJson(nodeData, JsonPtr"/class", TargetNames)))
+    # result = TargetNames(class)
+    result = newAssignment(ident"result", newLit(fromJson(nodeData, JsonPtr"/class", TargetNames)))
   else:
     result = newNimNode(nnkIfStmt)
+    # if input[feature] <= threshold'f32:
     result.add(newTree(nnkElifBranch,
         infix(newTree(nnkBracketExpr, input, newLit(fromJson(nodeData, JsonPtr"/feature", int))),
         "<=", newLit(fromJson(nodeData, JsonPtr"/threshold", float32))),
         generateNode(extract(nodeData, JsonPtr"/left"), input)))
+    # else:
     result.add(newTree(nnkElse, generateNode(extract(nodeData, JsonPtr"/right"), input)))
 
 macro generateClassifier(classifierData: static JsonTree): untyped =
@@ -26,19 +28,26 @@ macro generateClassifier(classifierData: static JsonTree): untyped =
   for treeData in items(classifierData, JsonPtr"", JsonTree):
     let input2 = genSym(nskParam, "input")
     let estimator = genSym(nskProc, "predict")
+    # proc predict(input: openarray[float32]): TargetNames =
     result.add newProc(name = estimator,
         params = [bindSym"TargetNames", newIdentDefs(name = input2,
-        kind = newTree(nnkBracketExpr, bindSym"seq", bindSym"float32"))],
+        kind = newTree(nnkBracketExpr, bindSym"openarray", bindSym"float32"))],
         body = generateNode(extract(treeData, JsonPtr"/node"), input2))
+    # inc(count[predict(input)])
     voting.add newCall(bindSym"inc", newTree(nnkBracketExpr, counter, newCall(estimator, input)))
+  # proc predict(input: openarray[float32]): TargetNames =
   result.add newProc(name = ident"predict", params = [bindSym"TargetNames",
-      newIdentDefs(name = input, kind = newTree(nnkBracketExpr, bindSym"seq", bindSym"float32"))],
+      newIdentDefs(name = input, kind = newTree(nnkBracketExpr, bindSym"openarray", bindSym"float32"))],
       body = newStmtList(newTree(nnkVarSection,
+        # var count: array[TargetNames, int32]
         newIdentDefs(counter, newTree(nnkBracketExpr, bindSym"array", bindSym"TargetNames", bindSym"int32"))),
         voting,
+        # result = TargetNames(maxIndex(count))
         newAssignment(ident"result", newCall(bindSym"TargetNames", newCall(bindSym"maxIndex", counter)))))
   echo result.repr
 
 generateClassifier(classifier)
 
-echo predict(@[4.9'f32,3.1,1.5,0.1]) # == setosa
+# echo predict(@[4.9'f32,3.1,1.5,0.1]) # == setosa
+# echo predict(@[6.7'f32,3.0,5.0,1.7]) # == versicolor
+# echo predict(@[7.7'f32,2.6,6.9,2.3]) # == virginica
