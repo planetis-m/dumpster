3b6562a0ff010e665684ff71e1fec22a58cf0734
Antonis Geralis <b3lievem@yandex.com>
2024-04-11 13:37:57 +0300

    score rforest

--- classical_ml/random_forest.nim
+++ classical_ml/random_forest.nim
@@ -1,4 +1,6 @@
-import jsonpak, jsonpak/[builder, parser, jsonptr, extra], std/[macros, sequtils]
+import jsonpak, jsonpak/[builder, parser, jsonptr, extra]
+import std/[macros, parsecsv, strutils, random]
+from std/sequtils import maxIndex
 
 type
   TargetNames = enum
@@ -37,7 +39,7 @@ macro generateClassifier(classifierData: static JsonTree): untyped =
     # inc(count[predict(input)])
     voting.add newCall(bindSym"inc", newTree(nnkBracketExpr, counter, newCall(estimator, input)))
   # proc predict(input: openarray[float32]): TargetNames =
-  result.add newProc(name = ident"predict", params = [bindSym"TargetNames",
+  result.add newProc(name = postfix(ident"predict", "*"), params = [bindSym"TargetNames",
       newIdentDefs(name = input, kind = newTree(nnkBracketExpr, bindSym"openarray", bindSym"float32"))],
       body = newStmtList(newTree(nnkVarSection,
         # var count: array[TargetNames, int32]
@@ -45,10 +47,6 @@ macro generateClassifier(classifierData: static JsonTree): untyped =
         voting,
         # result = TargetNames(maxIndex(count))
         newAssignment(ident"result", newCall(bindSym"TargetNames", newCall(bindSym"maxIndex", counter)))))
-  echo result.repr
+  # echo result.repr
 
 generateClassifier(classifier)
-
-# echo predict(@[4.9'f32,3.1,1.5,0.1]) # == setosa
-# echo predict(@[6.7'f32,3.0,5.0,1.7]) # == versicolor
-# echo predict(@[7.7'f32,2.6,6.9,2.3]) # == virginica
---
+++ classical_ml/score_forest.nim
@@ -0,0 +1,47 @@
+import random_forest, std/[parsecsv, strutils, random]
+
+const
+  IrisDataLen = 150
+  IrisFeatures = 4
+  IrisLabels = [
+    "Iris-setosa",
+    "Iris-versicolor",
+    "Iris-virginica"
+  ]
+
+type
+  Features = array[IrisFeatures, float32]
+  DataSet = object
+    xs: seq[Features]
+    ys: seq[int32]
+
+proc readIrisData(): DataSet =
+  var p: CsvParser
+  try:
+    p.open("iris.data")
+    var xs = newSeq[Features](IrisDataLen)
+    var ys = newSeq[int32](IrisDataLen)
+    var x = 0
+    while p.readRow():
+      for y in 0..<IrisFeatures:
+        xs[x][y] = parseFloat(p.row[y])
+      ys[x] = find(IrisLabels, p.row[^1]).int32
+      inc x
+    assert x == IrisDataLen
+    result = DataSet(xs: xs, ys: ys)
+  finally:
+    p.close()
+
+proc accuracy(X: seq[Features], yTrue: seq[int32]): float32 =
+  var numCorrect = 0
+  for i in 0..<IrisDataLen:
+    if yTrue[i] == predict(X[i]).int32:
+      inc numCorrect
+  result = (numCorrect.float32/IrisDataLen)*100
+
+proc main =
+  randomize(123)
+  let iris = readIrisData()
+  echo "Accuracy: ", accuracy(iris.xs, iris.ys), "%"
+
+main()
