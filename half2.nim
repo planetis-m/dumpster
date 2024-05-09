652bb07fd33689c138dff539230ef5a6f499cadb
Antonis Geralis <b3lievem@yandex.com>
2024-05-09 07:21:00 +0300

    fixed

--- half2.nim
+++ half2.nim
@@ -4,10 +4,14 @@ type
 proc fromHalf*(x: float16): float32 {.importc: "(NF32)", nodecl.}
 proc toHalf*(x: float32): float16 {.importc: "(_Float16)", nodecl.}
 
-proc `+`*(a, b: float16): float16 {.importc, nodecl.}
-proc `-`*(a, b: float16): float16 {.importc, nodecl.}
-proc `*`*(a, b: float16): float16 {.importc, nodecl.}
-proc `/`*(a, b: float16): float16 {.importc, nodecl.}
+proc `+`*(a, b: float16): float16 {.inline.} =
+  {.emit: [result, " = ", a, " + ", b, ";"].}
+proc `-`*(a, b: float16): float16 {.inline.} =
+  {.emit: [result, " = ", a, " - ", b, ";"].}
+proc `*`*(a, b: float16): float16 {.inline.} =
+  {.emit: [result, " = ", a, " * ", b, ";"].}
+proc `/`*(a, b: float16): float16 {.inline.} =
+  {.emit: [result, " = ", a, " / ", b, ";"].}
 
 when isMainModule:
   let a = toHalf(0.125'f32)
