9681870942bb16689f06106401cbce5eef5c3a15
Antonis Geralis <b3lievem@yandex.com>
2024-05-13 12:58:43 +0300

    experiment with quine

--- hackmud/hackmud.nim
+++ hackmud/hackmud.nim
@@ -11,7 +11,8 @@ type
     this_script: cstring
     calling_script: cstring
 
-proc getStdLib(): StdLib {.importc: "#fs.scripts.lib".}
+# note: manually add the '#' prefix after minifying
+proc getStdLib(): StdLib {.importc: "fs.scripts.lib".}
 
 proc ok(s: StdLib): JsonNode {.importcpp.}
 proc notImpl(s: StdLib): JsonNode {.importcpp: "#.not_impl()".}
@@ -19,7 +20,9 @@ proc notImpl(s: StdLib): JsonNode {.importcpp: "#.not_impl()".}
 proc log(s: StdLib; message: cstring) {.importcpp.}
 proc getLog(s: StdLib): JSeq {.importcpp: "#.get_log()".}
 
-proc debug[T](ob: T) {.importc: "#D".}
+proc quine(): cstring {.importc: "fs.scripts.quine".}
+
+proc debug[T](ob: T) {.importc: "D".}
 
 proc crackEz21(c: Context; args: JsonNode): JsonNode {.exportc.} =
   ## Usage: script {target: #s.some_user.their_loc}
@@ -57,11 +60,43 @@ proc crackEz40(c: Context; args: JsonNode): JsonNode {.exportc.} =
     for a in attempts:
       let v = %*{ez_40: a}
       ret = target.call(v)
-      if not ret.contains(a):
+      if not ret.contains("\"" & a):
         cmd = a
         break
     for p in primes:
       let v = %*{ez_40: cmd, ez_prime: p}
+      ret = target.call(v)
+      if ret.contains("LOCK_UNLOCKED"):
+        # std.log("Correct password for ez_40 lock: " & p.toCstr)
+        success = true
+        break
+  result = %*{
+    ok: success,
+    msg: ret
+  }
+
+const
+  data = """
+{"a":["open","release","unlock"],"p":[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]}"""
+
+proc crackEz40Short(c: Context; args: JsonNode): JsonNode {.exportc.} =
+  ## Usage: script {target: #s.some_user.their_loc}
+  {.emit: ["//", data, "//"].}
+  # let std = getStdLib()
+  let target = cast[Target](args["target"])
+  var ret = target.call(JsonNode())
+  var success = false
+  if ret.contains("EZ_40"):
+    let data = parse(quine().split("//")[1]) # note: manually escape '/' after minifying
+    var cmd = cstring""
+    for a in data["a"].items:
+      let v = %*{ez_40: a.getStr}
+      ret = target.call(v)
+      if not ret.contains("\"" & a.getStr):
+        cmd = a.getStr
+        break
+    for p in data["p"].items:
+      let v = %*{ez_40: cmd, ez_prime: p.getInt}
       ret = target.call(v)
       if ret.contains("LOCK_UNLOCKED"):
         # std.log("Correct password for ez_40 lock: " & p.toCstr)
