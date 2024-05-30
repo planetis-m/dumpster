f67783901ef4a7667f433c72c4511cca50f7af90
Antonis Geralis <b3lievem@yandex.com>
2024-05-30 10:51:25 +0300

    random

--- randomness/squares.nim
+++ randomness/squares.nim
@@ -41,27 +41,78 @@ proc rand*(ctr: uint64; max: range[0'f32..high(float32)]): float32 =
   let u = (0x7f'u32 shl 23'u32) or (x shr 9'u32)
   result = (cast[float32](u) - 1'f32) * max
 
+# proc gauss*[T: SomeFloat](ctr: uint64; mu, sigma: T): T =
+#   # Ratio of uniforms method for normal
+#   # https://www2.econ.osaka-u.ac.jp/~tanizaki/class/2013/econome3/13.pdf
+#   const K = sqrt(2 / T(E))
+#   var
+#     a = T(0)
+#     b = T(0)
+#     ctr = ctr
+#   while true:
+#     a = rand(ctr, T(1))
+#     b = (T(2) * rand(ctr+1, T(1)) - T(1)) * K
+#     inc(ctr, 2)
+#     if  b * b <= -T(4) * a * a * ln(a): break
+#   result = mu + sigma * (b / a)
+
 proc gauss*[T: SomeFloat](ctr: uint64; mu, sigma: T): T =
-  # Ratio of uniforms method for normal
-  # https://www2.econ.osaka-u.ac.jp/~tanizaki/class/2013/econome3/13.pdf
-  const K = sqrt(2 / T(E))
   var
-    a = T(0)
-    b = T(0)
+    u1, u2, s, factor: T
     ctr = ctr
   while true:
-    a = rand(ctr, T(1))
-    b = (T(2) * rand(ctr+1, T(1)) - T(1)) * K
+    u1 = T(2) * rand(ctr, T(1)) - T(1)
+    u2 = T(2) * rand(ctr+1, T(1)) - T(1)
     inc(ctr, 2)
-    if  b * b <= -T(4) * a * a * ln(a): break
-  result = mu + sigma * (b / a)
+    s = u1 * u1 + u2 * u2
+    if s > T(0) and s < T(1): break
+  factor = sqrt(-T(2) * ln(s) / s)
+  result = mu + sigma * (u1 * factor)
+
+proc gauss*[T: SomeFloat](ctr: uint64; mu, sigma: T): (T, T) =
+  var
+    u1, u2, s, factor: T
+    ctr = ctr
+  while true:
+    u1 = T(2) * rand(ctr, T(1)) - T(1)
+    u2 = T(2) * rand(ctr+1, T(1)) - T(1)
+    inc(ctr, 2)
+    s = u1 * u1 + u2 * u2
+    if s > T(0) and s < T(1): break
+  factor = sqrt(-T(2) * ln(s) / s)
+  result = (mu + sigma * (u1 * factor), mu + sigma * (u2 * factor))
+
+type
+  GaussState*[T: SomeFloat] = object
+    u2: T
+    saved: bool
+
+proc gauss*[T: SomeFloat](state: var GaussState[T], ctr: var uint64; mu, sigma: T): T =
+  var
+    u1, u2, s, factor: T
+  if state.saved:
+    state.saved = false
+    result = mu + sigma * state.u2
+  else:
+    while true:
+      u1 = T(2) * rand(ctr, T(1)) - T(1)
+      u2 = T(2) * rand(ctr + 1, T(1)) - T(1)
+      inc(ctr, 2)
+      s = u1 * u1 + u2 * u2
+      if s > T(0) and s < T(1): break
+
+    factor = sqrt(-T(2) * ln(s) / s)
+    state.u2 = u2 * factor
+    state.saved = true
+    result = mu + sigma * (u1 * factor)
 
 import std/stats
 var rs: RunningStat
 for j in 1..5:
   for i in 1 .. 100_000:
     rs.push(gauss(123456789+i.uint64*1000, 0.0f, 1.0f))
-  doAssert abs(rs.mean-0) < 0.08, $rs.mean
+  echo (rs.mean, rs.standardDeviation())
+  doAssert abs(rs.mean) < 0.08, $rs.mean
   doAssert abs(rs.standardDeviation()-1.0) < 0.1
   let bounds = [3.5, 5.0]
   for a in [rs.max, -rs.min]:
