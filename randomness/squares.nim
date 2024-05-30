99204eaef048b91162caa7fec2a446ed808b9674
Antonis Geralis <b3lievem@yandex.com>
2024-05-30 12:10:55 +0300

    uncomment everything for indexing

--- randomness/squares.nim
+++ randomness/squares.nim
@@ -41,33 +41,33 @@ proc rand*(ctr: uint64; max: range[0'f32..high(float32)]): float32 =
   let u = (0x7f'u32 shl 23'u32) or (x shr 9'u32)
   result = (cast[float32](u) - 1'f32) * max
 
-# proc gauss*[T: SomeFloat](ctr: uint64; mu, sigma: T): T =
-#   # Ratio of uniforms method for normal
-#   # https://www2.econ.osaka-u.ac.jp/~tanizaki/class/2013/econome3/13.pdf
-#   const K = sqrt(2 / T(E))
-#   var
-#     a = T(0)
-#     b = T(0)
-#     ctr = ctr
-#   while true:
-#     a = rand(ctr, T(1))
-#     b = (T(2) * rand(ctr+1, T(1)) - T(1)) * K
-#     inc(ctr, 2)
-#     if  b * b <= -T(4) * a * a * ln(a): break
-#   result = mu + sigma * (b / a)
+proc gauss*[T: SomeFloat](ctr: uint64; mu, sigma: T): T =
+  # Ratio of uniforms method for normal
+  # https://www2.econ.osaka-u.ac.jp/~tanizaki/class/2013/econome3/13.pdf
+  const K = sqrt(2 / T(E))
+  var
+    a = T(0)
+    b = T(0)
+    ctr = ctr
+  while true:
+    a = rand(ctr, T(1))
+    b = (T(2) * rand(ctr+1, T(1)) - T(1)) * K
+    inc(ctr, 2)
+    if  b * b <= -T(4) * a * a * ln(a): break
+  result = mu + sigma * (b / a)
 
-# proc gauss*[T: SomeFloat](ctr: uint64; mu, sigma: T): T =
-#   var
-#     u1, u2, s, factor: T
-#     ctr = ctr
-#   while true:
-#     u1 = T(2) * rand(ctr, T(1)) - T(1)
-#     u2 = T(2) * rand(ctr+1, T(1)) - T(1)
-#     inc(ctr, 2)
-#     s = u1 * u1 + u2 * u2
-#     if s > T(0) and s < T(1): break
-#   factor = sqrt(-T(2) * ln(s) / s)
-#   result = mu + sigma * (u1 * factor)
+proc gauss*[T: SomeFloat](ctr: uint64; mu, sigma: T): T =
+  var
+    u1, u2, s, factor: T
+    ctr = ctr
+  while true:
+    u1 = T(2) * rand(ctr, T(1)) - T(1)
+    u2 = T(2) * rand(ctr+1, T(1)) - T(1)
+    inc(ctr, 2)
+    s = u1 * u1 + u2 * u2
+    if s > T(0) and s < T(1): break
+  factor = sqrt(-T(2) * ln(s) / s)
+  result = mu + sigma * (u1 * factor)
 
 proc gauss*[T: SomeFloat](ctr: uint64; mu, sigma: T): (T, T) =
   var
@@ -107,48 +107,48 @@ proc gauss*[T: SomeFloat](state: var GaussState[T], ctr: uint64; mu, sigma: T): 
     state.saved = true
     result = mu + sigma * (u1 * factor)
 
-# type
-#   BoxMullerState*[T] = object
-#     saved: bool
-#     z1: T
-#
-# proc gauss*[T: SomeFloat](state: var BoxMullerState[T], ctr: uint64, mu, sigma: T): T =
-#   var
-#     u1, u2, r, theta: T
-#     ctr = ctr
-#   if state.saved:
-#     state.saved = false
-#     result = mu + sigma * state.z1
-#   else:
-#     while true:
-#       u1 = rand(ctr, T(1))
-#       u2 = rand(ctr, T(1))
-#       inc(ctr, 2)
-#       if u1 > T(0): break
-#
-#     r = sqrt(-T(2) * ln(u1))
-#     theta = T(TAU) * u2
-#     let z0 = r * cos(theta)
-#     state.z1 = r * sin(theta)
-#     state.saved = true
-#     result = mu + sigma * z0
-#
-# proc gauss*[T: SomeFloat](ctr: uint64, mu, sigma: T): (T, T) =
-#   var
-#     u1, u2, r, theta: T
-#     ctr = ctr
-#   while true:
-#     u1 = rand(ctr, T(1))
-#     u2 = rand(ctr, T(1))
-#     if u1 > T(0): break
-#   inc(ctr, 2)
-#
-#   r = sqrt(-T(2) * ln(u1))
-#   theta = T(TAU) * u2
-#   let z0 = r * cos(theta)
-#   let z1 = r * sin(theta)
-#
-#   result = (mu + sigma * z0, mu + sigma * z1)
+type
+  BoxMullerState*[T] = object
+    saved: bool
+    z1: T
+
+proc gauss*[T: SomeFloat](state: var BoxMullerState[T], ctr: uint64, mu, sigma: T): T =
+  var
+    u1, u2, r, theta: T
+    ctr = ctr
+  if state.saved:
+    state.saved = false
+    result = mu + sigma * state.z1
+  else:
+    while true:
+      u1 = rand(ctr, T(1))
+      u2 = rand(ctr, T(1))
+      inc(ctr, 2)
+      if u1 > T(0): break
+
+    r = sqrt(-T(2) * ln(u1))
+    theta = T(TAU) * u2
+    let z0 = r * cos(theta)
+    state.z1 = r * sin(theta)
+    state.saved = true
+    result = mu + sigma * z0
+
+proc gauss*[T: SomeFloat](ctr: uint64, mu, sigma: T): (T, T) =
+  var
+    u1, u2, r, theta: T
+    ctr = ctr
+  while true:
+    u1 = rand(ctr, T(1))
+    u2 = rand(ctr, T(1))
+    if u1 > T(0): break
+  inc(ctr, 2)
+
+  r = sqrt(-T(2) * ln(u1))
+  theta = T(TAU) * u2
+  let z0 = r * cos(theta)
+  let z1 = r * sin(theta)
+
+  result = (mu + sigma * z0, mu + sigma * z1)
 
 import std/stats
 var rs: RunningStat
