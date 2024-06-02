685b0b125719eaa8c9380fdcfd0aad75f0fd9b49
Antonis Geralis <b3lievem@yandex.com>
2024-06-02 03:08:39 +0300

    damn

--- randomness/stateless_shuffle2.nim
+++ randomness/stateless_shuffle2.nim
@@ -12,14 +12,14 @@ proc getRequiredBits(len: Natural): uint32 {.inline.} =
 
 const
   Rounds = 10
-  Len = 23
+  Len = 32
   BitWidth = getRequiredBits(Len) # Bit width of the input
   BitMask = (1'u32 shl BitWidth) - 1'u32
 
 proc genKeySet(keys: var openarray[uint32]) =
   # Generate a cryptographically secure random key set
   const size = ceilDiv(BitWidth, 8) # Calculate the number of bytes needed for the width
-  for i in 0..(Rounds div 2):
+  for i in 0..keys.high:
     var value: uint32 = 0
     when defined(trueRandom):
       let bytes = urandom(size)
@@ -27,11 +27,10 @@ proc genKeySet(keys: var openarray[uint32]) =
         value = (value shl 8) or uint32(bytes[j])
     else:
       value = rand(uint32)
-    keys[i] = masked(value, BitMask - 2)
-    # keys[i] = value
+    keys[i] = masked(value, BitMask)
 
 var
-  KeySet: array[Rounds div 2 + 1, uint32]
+  KeySet: array[Rounds + 1, uint32]
 
 proc rotateLeft(x, bits, BitWidth: uint32): uint32 {.inline.} =
   (x shl bits) or (x shr (BitWidth - bits))
@@ -48,6 +47,25 @@ proc arrhrEncrypt(x: uint32): uint32 =
 
 proc arrhrDecrypt(y: uint32): uint32 =
   result = (y - KeySet[Rounds div 2]) and BitMask
+  for i in countdown(Rounds div 2 - 1, 0):
+    result = rotateRight(result, 1, BitWidth)
+    result = (result - KeySet[i]) and BitMask
+
+proc arrrEncrypt(x: uint32): uint32 =
+  result = x
+  for i in 0..<(Rounds div 2):
+    result = (result + KeySet[i]) and BitMask
+    result = rotateLeft(result, 1, BitWidth)
+  for i in (Rounds div 2)..<Rounds:
+    result = (result + KeySet[i]) and BitMask
+    result = rotateRight(result, 1, BitWidth)
+  result = (result + KeySet[Rounds]) and BitMask
+
+proc arrrDecrypt(y: uint32): uint32 =
+  result = (y - KeySet[Rounds]) and BitMask
+  for i in countdown(Rounds - 1, Rounds div 2):
+    result = rotateLeft(result, 1, BitWidth)
+    result = (result - KeySet[i]) and BitMask
   for i in countdown(Rounds div 2 - 1, 0):
     result = rotateRight(result, 1, BitWidth)
     result = (result - KeySet[i]) and BitMask
@@ -90,9 +108,10 @@ proc frequencyTest() =
 
   let expectedFrequency = NumIters div Len
   let tolerance = expectedFrequency.float / 4 # 25% tolerance
-
+  echo tolerance
   for i in 0..<Len:
     for j in 0..<Len:
+      echo abs(frequencies[i][j] - expectedFrequency).float
       doAssert abs(frequencies[i][j] - expectedFrequency).float <= tolerance, "Frequency test failed"
 
 # frequencyTest()
