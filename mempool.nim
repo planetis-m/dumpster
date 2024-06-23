eabf6c9ec04d439b2054c09f1ca88e799b28dff3
Antonis Geralis <b3lievem@yandex.com>
2024-06-24 01:27:59 +0300

    more

--- mempool.nim
+++ mempool.nim
@@ -48,3 +48,69 @@ proc dealloc*(x: var FixedPool, p: pointer) =
     cast[ptr uint32](p)[] = x.numOfBlocks
     x.next = index
   inc(x.numFreeBlocks)
+
+when isMainModule:
+  import std/strformat
+
+  type
+    Vector2D = object
+      x, y: float32
+
+  proc main() =
+    const
+      PoolSize = 5
+
+    # Create the object pool
+    var pool = createFixedPool(sizeof(Vector2D), PoolSize)
+
+    echo "Initial pool state:"
+    echo(pool)
+
+    # Allocate some vectors
+    var vectors: array[PoolSize, ptr Vector2D]
+    for i in 0..<PoolSize:
+      vectors[i] = cast[ptr Vector2D](alloc(pool))
+      if vectors[i] != nil:
+        vectors[i].x = float32(i)
+        vectors[i].y = float32(i * i)
+        echo &"Allocated vector {i}: ", vectors[i][]
+
+    echo "\nAfter allocating all vectors:"
+    echo(pool)
+
+    # Free some vectors
+    echo "\nFreeing vectors 1 and 3"
+    dealloc(pool, vectors[1])
+    dealloc(pool, vectors[3])
+
+    echo "\nAfter freeing two vectors:"
+    echo(pool)
+
+    # Allocate new vectors, which should reuse the freed memory
+    var newVector1 = cast[ptr Vector2D](alloc(pool))
+    var newVector2 = cast[ptr Vector2D](alloc(pool))
+
+    if newVector1 != nil:
+      newVector1.x = 10.5
+      newVector1.y = 20.7
+      echo "\nAllocated new vector 1:"
+      echo(newVector1[])
+
+    if newVector2 != nil:
+      newVector2.x = -5.2
+      newVector2.y = 15.3
+      echo "Allocated new vector 2:"
+      echo(newVector2[])
+
+    echo "\nFinal pool state:"
+    echo(pool)
+
+    # Try to allocate one more vector (should fail)
+    var extraVector = cast[ptr Vector2D](alloc(pool))
+    if extraVector == nil:
+      echo "Failed to allocate extra vector (pool is full)"
+    else:
+      echo "Unexpectedly allocated extra vector"
+
+  # Run the example
+  main()
---
+++ structures/pool.nim
@@ -0,0 +1,102 @@
+# https://www.gingerbill.org/code/memory-allocation-strategies/part004.c
+
+const DEFAULT_ALIGNMENT = 8
+
+type
+  FixedPoolFreeNode = object
+    next: ptr FixedPoolFreeNode
+
+  FixedPool*[T] = object
+    buf: ptr UncheckedArray[byte]
+    bufLen: int
+    chunkSize: int
+    head: ptr FixedPoolFreeNode  # Free List Head
+
+proc alignup(n, align: uint): uint {.inline.} =
+  (n + align - 1) and (not (align - 1))
+
+proc deallocAll*(x: var FixedPool)
+
+proc init*[T](x: var FixedPool[T], buffer: openarray[byte]) =
+  let start = cast[uint](buffer)
+  let alignedStart = alignup(start, alignof(T).uint)
+  let alignedLen = buffer.len - int(alignedStart - start)
+  # Align chunk size up to the required chunkAlignment
+  let alignedSize = alignup(sizeof(T).uint, alignof(T).uint).int
+  # Assert that the parameters passed are valid
+  assert alignedSize >= sizeof(FixedPoolFreeNode), "Chunk size is too small"
+  assert alignedLen >= alignedSize, "Backing buffer length is smaller than the chunk size"
+  # Store the adjusted parameters
+  x.buf = cast[ptr UncheckedArray[byte]](alignedStart)
+  x.bufLen = alignedLen
+  x.chunkSize = alignedSize
+  x.head = nil # Free List Head
+  # Set up the free list for free chunks
+  deallocAll(x)
+
+proc alloc*[T](x: var FixedPool[T]): ptr T =
+  # Get latest free node
+  let node = x.head
+  if node == nil:
+    assert false, "FixedPool allocator has no free memory"
+    return nil
+  # Pop free node
+  x.head = node.next
+  # Zero memory by default
+  zeroMem(node, x.chunkSize)
+  result = cast[ptr T](node)
+
+proc dealloc*[T](x: var FixedPool[T], p: ptr T) =
+  if p == nil:
+    # Ignore NULL pointers
+    return
+  let start = cast[uint](x.buf)
+  let endAddr = start + uint(x.bufLen)
+  if start > cast[uint](p) or cast[uint](p) >= endAddr:
+    assert false, "Memory is out of bounds of the buffer in this pool"
+    return
+  # Push free node
+  let node = cast[ptr FixedPoolFreeNode](p)
+  node.next = x.head
+  x.head = node
+
+proc deallocAll*(x: var FixedPool) =
+  let chunkCount = x.bufLen div x.chunkSize
+  # Set all chunks to be free
+  for i in 0 ..< chunkCount:
+    let p = cast[pointer](cast[uint](x.buf) + uint(i * x.chunkSize))
+    let node = cast[ptr FixedPoolFreeNode](p)
+    # Push free node onto the free list
+    node.next = x.head
+    x.head = node
+
+when isMainModule:
+  type
+    Vector2D = object
+      x, y: float32
+
+  var backingBuffer: array[1024, byte]
+  var x: FixedPool[Vector2D]
+  init(x, backingBuffer)
+
+  var a = alloc(x)
+  var b = alloc(x)
+  var c = alloc(x)
+  var d = alloc(x)
+  var e = alloc(x)
+  var f = alloc(x)
+
+  dealloc(x, f)
+  dealloc(x, c)
+  dealloc(x, b)
+  dealloc(x, d)
+
+  d = alloc(x)
+
+  dealloc(x, a)
+
+  a = alloc(x)
+
+  dealloc(x, e)
+  dealloc(x, a)
+  dealloc(x, d)
